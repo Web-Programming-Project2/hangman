53963d8c242237bc36fbf356eeef1ecec057faca
jt <47215199+tvrjuana@users.noreply.github.com>
2021-03-18 14:57:59 -0400

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,27 @@
+<?php
+    //signup page for new users
+    include("common.php");
+    top();
+?>
+
+    <form action = "signup-submit.php" method = "post">
+        <!--Form containing the fields: username, password, and confirm password -->
+        <fieldset class = "column">
+            <legend>New User Signup:</legend>
+
+                <label class = "heading" for = "name">Username: </label>
+                <input type = "text" name = "name" size = "16"> <br>
+
+                <label class = "heading" for = "pw">Password: </label>
+                <input type = "text" name = "pw" size = "16"> <br>
+
+                <label class = "heading" for = "cpw">Confirm Password: </label>
+                <input type = "text" name = "cpw" size = "16"> <br>
+
+            <!--Submit button -->
+            <input type = "submit" value = "signup">
+        </fieldset>
+    </form>
+
+</body>
+</html>
