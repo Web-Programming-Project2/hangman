a400946b23ff6df953bcc224ff5e8c7f1d1e19d7
jt <47215199+tvrjuana@users.noreply.github.com>
2021-03-22 20:39:33 -0400

    Delete create.sql

--- create.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `webprogramming_p2`.`users` (
-  `username` VARCHAR(16) NOT NULL,
-  `password` VARCHAR(16) NULL,
-  PRIMARY KEY (`username`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);
