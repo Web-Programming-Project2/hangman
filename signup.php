3cb4a85addcd66cb85d58926d93b75164813644e
jt <47215199+tvrjuana@users.noreply.github.com>
2021-03-22 20:37:08 -0400

    added php code

--- signup.php
+++ signup.php
@@ -4,8 +4,8 @@
     top();
 ?>
 
-    <form action = "signup-submit.php" method = "post">
-        <!--Form containing the fields: username, password, and confirm password -->
+    <form action = "signup.php" method = "post">
+        <!--Form containing the fields: username and password -->
         <fieldset class = "column">
             <legend>New User Signup:</legend>
 
@@ -15,13 +15,32 @@
                 <label class = "heading" for = "password">Password: </label>
                 <input type = "password" name = "password" size = "16"> <br> <br>
 
-                <label class = "heading" for = "confirm">Confirm Password: </label>
-                <input type = "password" name = "confirm" size = "16"> <br> <br>
-
             <!--Submit button -->
-            <input type = "submit" value = "signup">
+            <input type = "submit" name = "submit" value = "signup">
         </fieldset>
     </form>
 
 </body>
 </html>
+
+<?php
+    if(isset($_POST['submit'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $file = file_get_contents("users.txt");
+        $string = "$username||$password";
+        
+        if(!strstr($file, "$string")){
+            $myFile = "users.txt";
+            $fh = fopen($myFile, 'a') or die("can't open file");
+            $stringData = "$username||$password";
+            $newLine = $stringData . "\n";
+            fwrite($fh, $newLine);
+            echo "Signup completed successfully";
+            fclose($fh);
+        }else{
+            echo "Username already in use";
+        }
+    }
+?>
