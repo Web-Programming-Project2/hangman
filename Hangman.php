3a533a19c1d60c5fa10a13ac1556a1a6357dcda0
Rachel Powers <rae.powers621@gmail.com>
2021-03-25 20:03:15 -0400

    Added in a score system

--- Hangman.php
+++ Hangman.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -8,7 +8,7 @@
   <body>
     <div id="circle" >
         <div class="a">
-      <a href="difficulty.php" id="back"> <- Back to Difficulty page</a>
+      <a href="login.php" id="back"> <- Back to Login</a>
 
       <div class="main">
 <?php
@@ -17,12 +17,13 @@ require_once 'hangedman.php';
 
 
 
+
 $words = array();
 $hints = array();
 $numwords = 0;
-$points = array();
 
-function printPage($image, $guesstemplate, $which, $guessed, $wrong, $points) {
+
+function printPage($image, $guesstemplate, $which, $guessed, $wrong, $score, $gameCount) {
   $user =$_GET["username"];
   echo "Player: $user";
 
@@ -44,7 +45,8 @@ function printPage($image, $guesstemplate, $which, $guessed, $wrong, $points) {
 	<input type="hidden" name="wrong" value="$wrong" />
 	<input type="hidden" name="lettersguessed" value="$guessed" />
 	<input type="hidden" name="word" value="$which" />
-  <input type="hidden" name="point" value="$points" />
+  <input type="hidden" name="score" value="$score" />
+  <input type="hidden" name="count" value="$gameCount" />
 	<fieldset>
 	  <legend>Your next guess</legend>
 	  <input type="text" name="letter"  />
@@ -56,7 +58,6 @@ function printPage($image, $guesstemplate, $which, $guessed, $wrong, $points) {
 ENDPAGE;
 
 global $hints;
-global $points;
 
 $which =$_POST["word"];
 $hint  = $hints[$which];
@@ -68,9 +69,9 @@ print_r($hint);
 echo '<br>';
 echo '<br>';
 
-$sum = array_sum($points);
 echo 'points: ';
-echo $sum;
+
+echo $_POST['score'];
 }
 
 function loadWords() {
@@ -107,28 +108,43 @@ function loadWords() {
   }
   fclose($input);
 }
-
-function startGame() {
+function repeatGame() {
   global $words;
   global $numwords;
   global $hang;
-  global $points;
 
   $which = rand(0, $numwords - 1);
   $word =  $words[$which];
   $len = strlen($word);
   $guesstemplate = str_repeat('_ ', $len);
   $script = $_SERVER["PHP_SELF"];
-  $point = $_POST["point"];
+  $points = $_POST['score'];
+  $count =$_POST['count'];
+  $count++;
+
+  printPage($hang[0], $guesstemplate, $which, "", 0,$points,$count);
+}
+function startGame() {
+  global $words;
+  global $numwords;
+  global $hang;
 
 
-  printPage($hang[0], $guesstemplate, $which, "", 0, $point);
+  $which = rand(0, $numwords - 1);
+  $word =  $words[$which];
+  $len = strlen($word);
+  $guesstemplate = str_repeat('_ ', $len);
+  $script = $_SERVER["PHP_SELF"];
+
+
+
+  printPage($hang[0], $guesstemplate, $which, "", 0,0,1);
 }
 
 function killPlayer($word) {
-  global $points;
   $user =$_GET["username"];
   $pw =$_GET["password"];
+  $score = $_POST['score'];
 
   echo "$user you lost!";
   echo <<<ENDPAGE
@@ -139,35 +155,7 @@ function killPlayer($word) {
   </head>
   <body>
   <form action= "leaderboard.php" method="get" class="form">
-	<p>The word you were trying to guess was <em>$word</em>.</p>
-  <input type="hidden" name="username" value= "$user"/>
-  <input type="hidden" name="password" value= "$pw"/>
-  <input type="hidden" name="score" value= "$points"/>
-  <br>
-  <br>
-  <br>
-  <input type = "submit" value = "Check the leaderboard">
-  </form>
-  </body>
-</html>
-ENDPAGE;
-}
-
-function congratulateWinner($word) {
-  global $points;
-  $score = $points[0];
-  $user =$_GET["username"];
-  $pw =$_GET["password"];
-  echo "$user you won!";
-  echo <<<ENDPAGE
-<!DOCTYPE html>
-<html>
-  <head>
-	<title>Hangman</title>
-  </head>
-  <body>
-  <form action= "leaderboard.php" method="get" class="form">
-	<p>Congratulations! You guessed that the word was <em>$word</em>.</p>
+	<p>"Congrats, $user you guessed $word correctly!</p>
   <input type="hidden" name="username" value= "$user"/>
   <input type="hidden" name="password" value= "$pw"/>
   <input type="hidden" name="score" value= "$score"/>
@@ -179,7 +167,44 @@ function congratulateWinner($word) {
   </body>
 </html>
 ENDPAGE;
-	
+}
+
+function congratulateWinner($word) {
+$user =$_GET["username"];
+echo "Congrats, $user you guessed $word correctly! Lets play again!<br><br>";
+$points = $_POST['score'];
+$points++;
+$_POST['score']= $points;
+repeatGame();
+
+}
+
+function endGameWinner(){
+  $user =$_GET["username"];
+  $pw =$_GET["password"];
+  $score = $_POST['score'];
+
+  echo "Congrats $user you this level!";
+  echo <<<ENDPAGE
+<!DOCTYPE html>
+<html>
+ <head>
+	<title>Hangman</title>
+  </head>
+  <body>
+  <form action= "leaderboard.php" method="get" class="form">
+	<p>The word you were trying to guess was <em>$word</em>.</p>
+  <input type="hidden" name="username" value= "$user"/>
+  <input type="hidden" name="password" value= "$pw"/>
+  <input type="hidden" name="score" value= "$score"/>
+  <br>
+  <br>
+  <br>
+  <input type = "submit" value = "Check the leaderboard">
+  </form>
+  </body>
+</html>
+ENDPAGE;
 }
 
 function matchLetters($word, $guessedLetters) {
@@ -207,7 +232,7 @@ function matchLetters($word, $guessedLetters) {
 function handleGuess() {
   global $words;
   global $hang;
-  global $points;
+
 
   $which = $_POST["word"];
   $word  = $words[$which];
@@ -215,26 +240,25 @@ function handleGuess() {
   $lettersguessed = $_POST["lettersguessed"];
   $guess = $_POST["letter"];
   $letter = $guess[0];
-  $point = $_POST["point"];
-
+  $points = $_POST['score'];
+  $count = $_POST['count'];
+  $gameCount = count($words);
 
   if(!strstr($word, $letter)) {
-	$wrong++;
-  
-  }
-  else{
-  $points[]= 1;
+	   $wrong++;
   }
 
   $lettersguessed = $lettersguessed . $letter;
   $guesstemplate = matchLetters($word, $lettersguessed);
 
-  if (!strstr($guesstemplate, "_")) {
+  if ((!strstr($guesstemplate, "_")) && ($count!=$gameCount)) {
    	congratulateWinner($word);
-  } else if ($wrong >= 6) {
-	killPlayer($word);
-  } else {
-	printPage($hang[$wrong], $guesstemplate, $which, $lettersguessed, $wrong, $points);
+  } else if (($wrong >= 6) && ($count!=$gameCount)) {
+	   killPlayer($word);
+  } else if((!strstr($guesstemplate, "_"))&& ($count==$gameCount) ){
+    endGameWinner();
+  }else {
+  	printPage($hang[$wrong], $guesstemplate, $which, $lettersguessed, $wrong, $points,$count);
   }
 }
 
